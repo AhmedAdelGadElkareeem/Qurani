79a17f3c9c79a187bfa03064a4320f3d09a0d39c
AShaaban <shabanahmed762@gmail.com>
2025-03-07 19:51:41 +0200

    .

--- WytSky.Mobile.Maui.Skoola/APIs/ServiceCenter.cs
+++ WytSky.Mobile.Maui.Skoola/APIs/ServiceCenter.cs
@@ -37,6 +37,33 @@ namespace WytSky.Mobile.Maui.Skoola.APIs
                 return null;
             }
         }
+        public async static Task<ObservableCollection<CentersModel>> GetCenters()
+        {
+            try
+            {
+                var dictionary = new Dictionary<string, string>()
+                {
+                      {"_datatype", "json"},
+                      {"_jsonarray", "1"},
+                };
+                var result = await Services.RequestProvider.Current.GetData<TempletData<CentersModel>>(BASE, "centers", dictionary, Enums.AuthorizationType.UserNamePassword);
+                if (result != null && result.IsPassed)
+                {
+                    return result.Data.ItemData;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                string ExceptionMseeage = string.Format(" Error : {0} - {1} ", ex.Message, ex.InnerException != null ? ex.InnerException.FullMessage() : "");
+                System.Diagnostics.Debug.WriteLine(ExceptionMseeage);
+                ExtensionLogMethods.LogExtension(ExceptionMseeage, "", "ServiceCatgeory", "GetParentCategories");
+                return null;
+            }
+        }
 
 
 
--- WytSky.Mobile.Maui.Skoola/Models/CentersModel.cs
+++ WytSky.Mobile.Maui.Skoola/Models/CentersModel.cs
@@ -3,21 +3,21 @@ namespace WytSky.Mobile.Maui.Skoola.Models
 {
     public class CentersModel 
     {
-        public int CenterID { get; set; }
-        public int ComplexID { get; set; }
-        public string CenterName { get; set; }
-        public string CenterNameEn { get; set; }
-        public string Name { get{ return App.IsArabic ? CenterName : CenterNameEn; } }
-        public string Address { get; set; }
+        public int? CenterID { get; set; }
+        public int? ComplexID { get; set; }
+        public string? CenterName { get; set; }
+        public string? CenterNameEn { get; set; }
+        public string? Name { get{ return App.IsArabic ? CenterName : CenterNameEn; } }
+        public string? Address { get; set; }
         public bool? IsActive { get; set; }
-        public string City { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
+        public string? City { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
         public int? SupervisorID { get; set; }
-        public string Notes { get; set; }
+        public string? Notes { get; set; }
         public int? StudyGroupCount { get; set; }
-        public string ComplexName { get; set; }
-        public string ComplexRegionName { get; set; }
-        public string ComplexRegionCountryName { get; set; }
+        public string? ComplexName { get; set; }
+        public string? ComplexRegionName { get; set; }
+        public string? ComplexRegionCountryName { get; set; }
     }
 }
